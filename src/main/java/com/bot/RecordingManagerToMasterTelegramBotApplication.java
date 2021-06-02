9b7e27a36cd462d1799f9f02ab434d8d1ec19451
Bronx666 <Hristyuk@outlook.com>
2021-06-02 12:59:43 +0300

    Initial commit

---
+++ src/main/java/com/bot/RecordingManagerToMasterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordingManagerToMasterTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordingManagerToMasterTelegramBotApplication.class, args);
+	}
+
+}
